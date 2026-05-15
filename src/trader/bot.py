c69229665aaee28c86ac073b5c979709a3c3cb04
WW <212500581@qq.com>
2026-05-16 03:42:01 +0800

    Select v57 exit-tuned live model

--- src/trader/bot.py
+++ src/trader/bot.py
@@ -36,7 +36,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger("MemeBot")
 
-DEFAULT_LIVE_MODEL_DIR = "data/models/20260516_v56_live_selected_v50_thr0972_pf15_volume15"
+DEFAULT_LIVE_MODEL_DIR = "data/models/20260516_v57_exit_tuned_pf16_mh12_tr22_10"
 
 
 def _runtime_model_dir() -> str:
